c9c966e8d598f3302a50dbe6aa03ce8a0502b6ff
Diney Bomfim <dineybomfim@gmail.com>
2023-06-27 22:36:51 +0400

    Reorganization

---
+++ Fusion/Core/FusionCore.h
@@ -0,0 +1,11 @@
+//
+//  Created by Diney on 5/1/23.
+//
+
+#import <Foundation/Foundation.h>
+
+// MARK: - Definitions -
+
+// Framework identification.
+static const double kFusionCoreVersion = 1.0;
+static NSString *const kFusionCoreName = @"Fusion";
