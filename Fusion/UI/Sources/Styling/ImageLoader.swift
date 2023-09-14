31b13f417d8cf0857284bf104bfed51077b0d264
Diney Bomfim <dineybomfim@gmail.com>
2023-09-14 21:43:34 +0400

    Gif image loader

--- Fusion/UI/Sources/Styling/ImageLoader.swift
+++ Fusion/UI/Sources/Styling/ImageLoader.swift
@@ -87,7 +87,7 @@ public extension UIImage {
 	///   - gifData: The data of the GIF image.
 	/// - Returns: An animated UIImage if successful, or nil if there was an error.
 	static func images(gifData: Data) -> UIImage? {
-#if !os(watchOS)
+#if !os(watchOS) && !os(xrOS)
 		var duration: TimeInterval = 0.0
 		
 		guard let source = CGImageSourceCreateWithData(gifData as CFData, nil) else { return nil }
