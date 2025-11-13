e3c8c42e6b99cfd3ee0f4b75ad1d2144e0a88f5a
Diney Bomfim <>
2025-11-14 00:51:59 +0400

    - Color update

--- Fusion/UI/Sources/Styling/Colors.swift
+++ Fusion/UI/Sources/Styling/Colors.swift
@@ -235,6 +235,42 @@ public extension Color {
 		return color.alpha == 0 ? nil : self
 	}
 	
+	init(hex: String?) {
+		var hexSanitized = hex?
+			.trimmingCharacters(in: .whitespacesAndNewlines)
+			.replacingOccurrences(of: "0x", with: "")
+			.replacingOccurrences(of: "#", with: "") ?? ""
+		
+		var rgb: UInt64 = 0
+		guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else {
+			self = .black
+			return
+		}
+		
+		let length = hexSanitized.count
+		let r, g, b, a: Double
+		
+		switch length {
+		case 6:
+			r = Double((rgb & 0xFF0000) >> 16) / 255.0
+			g = Double((rgb & 0x00FF00) >> 8) / 255.0
+			b = Double(rgb & 0x0000FF) / 255.0
+			a = 1.0
+		case 8:
+			r = Double((rgb & 0xFF000000) >> 24) / 255.0
+			g = Double((rgb & 0x00FF0000) >> 16) / 255.0
+			b = Double((rgb & 0x0000FF00) >> 8) / 255.0
+			a = Double(rgb & 0x000000FF) / 255.0
+		default:
+			r = 0
+			g = 0
+			b = 0
+			a = 1
+		}
+		
+		self = Color(red: r, green: g, blue: b, opacity: a)
+	}
+	
 	private static func inAnyBundle(_ named: String) -> Color? {
 		Bundle.allAvailable.firstMap { .init(named, bundle: $0).nilIfClear } ?? .init(named)
 	}
