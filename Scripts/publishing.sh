0c6300c160ae399c9adc1a465b434d12e687c936
Diney Bomfim <926608+dineybomfim@users.noreply.github.com>
2026-02-22 11:11:48 +0400

    - Updating version

--- Scripts/publishing.sh
+++ Scripts/publishing.sh
@@ -18,7 +18,7 @@ else
 	FILE=$(find . -name *.podspec)
 	NAME=$(awk '/\.name =/' $FILE | sed 's/.*"\(.*\)"/\1/g')
 	VERSION=$(awk '/\.version/' $FILE | awk '/[0-9]\.[0-9]\.[0-9]/' | sed 's/.version//g' | sed 's/[^0-9/.]//g')
-	TAG="${NAME}-v${VERSION}"
+	TAG="${VERSION}"
 	git tag -a -f "${TAG}" -m "Pod version update" || true
 	git push origin -v "refs/tags/${TAG}" || true
 
