7513cf27d6748fea33169c55d2b28970176f960b
Diney Bomfim <926608+dineybomfim@users.noreply.github.com>
2026-07-09 14:43:38 +0400

    - String table

--- Fusion/Core/Sources/Controls/Localization.swift
+++ Fusion/Core/Sources/Controls/Localization.swift
@@ -124,7 +124,7 @@ public extension String {
 	private static var tableKey: UInt8 = 1
 	
 	/// Localizable Table strings.
-	static let localizableTable = "Localizable"
+	static var localizableTable = "Localizable.nocache"
 	
 	/// Returns the language code in ISO 639-1 format (2 alpha codes).
 	var codeISO2: String { prefix(2).lowercased() }
