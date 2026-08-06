8e84ac472fb5380bae85ce59e44e061af45af75a
Diney Bomfim <926608+dineybomfim@users.noreply.github.com>
2026-08-06 11:13:17 +0400

    - Race update

--- Fusion/Core/Sources/Storage/ThreadSafe.swift
+++ Fusion/Core/Sources/Storage/ThreadSafe.swift
@@ -20,22 +20,6 @@ public final class ThreadSafe<Value> {
 
 	public var wrappedValue: Value {
 		get { queue.sync(flags: .barrier) { value } }
-		set { queue.async(flags: .barrier) {  [weak self] in self?.value = newValue } }
-	}
-}
-
-@propertyWrapper
-public final class ThreadSafeSync<Value> {
-	
-	private var value: Value
-	private let queue = DispatchQueue(label: "\(UUID().uuidString)", attributes: .concurrent)
-
-	public init(wrappedValue: Value) {
-		self.value = wrappedValue
-	}
-
-	public var wrappedValue: Value {
-		get { queue.sync(flags: .barrier) { value } }
 		set { queue.sync(flags: .barrier) { value = newValue } }
 	}
 }
